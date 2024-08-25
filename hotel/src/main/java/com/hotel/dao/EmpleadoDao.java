1928cb476f9ab46fcd48c6cc7c2bb1208c35a27e
jose1409 <jaraya40805@ufide.ac.cr>
2024-08-25 01:00:48 -0600

    Update

--- hotel/src/main/java/com/hotel/dao/EmpleadoDao.java
+++ hotel/src/main/java/com/hotel/dao/EmpleadoDao.java
@@ -3,10 +3,6 @@ package com.hotel.dao;
 import com.hotel.domain.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- *
- * @author Usuario
- */
 public interface EmpleadoDAO extends JpaRepository<Empleado, Long>{
     
 }
